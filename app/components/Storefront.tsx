6ba0f2dd8ba986bdd230981c18c4b7430336c102
EldarMister <marselanarbekovv@gmail.com>
2026-07-27 02:08:22 +0600

    Use final generated mobile hero

--- app/components/Storefront.tsx
+++ app/components/Storefront.tsx
@@ -1244,11 +1244,6 @@ function StorefrontContent({ categorySlug }: { categorySlug?: string }) {
           <source media="(max-width: 720px)" srcSet="/mobile-header-1600-transparent-v2.png" />
           <img src="/desktop-header-20260726-v3.png" alt="Накта суши" />
         </picture>
-        <span className="mobile-brand-logo-fix" aria-hidden="true">
-          <i className="mobile-brand-fin mobile-brand-fin-top" />
-          <i className="mobile-brand-fin mobile-brand-fin-bottom" />
-          <i className="mobile-brand-diamond" />
-        </span>
         <img className="brand-smile" src="/смайлик.webp" alt="" aria-hidden="true" />
         <a href="https://trk.mail.ru/c/a7gh71" aria-label="Скачайте приложение"><img className="download-app" src="https://mnogolososya.ru/_nuxt/download-app.BLqCltS2.svg" alt="Скачайте приложение" /></a>
       </section>
