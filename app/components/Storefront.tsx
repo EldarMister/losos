9c22c4df0dbaf3f4038f6fb81245256453da6cb2
EldarMister <marselanarbekovv@gmail.com>
2026-08-13 20:38:55 +0600

    Fix profile authorization modal crash

--- app/components/Storefront.tsx
+++ app/components/Storefront.tsx
@@ -3,7 +3,7 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import Icon from "@mdi/react";
+import { Icon } from "@mdi/react";
 import {
   mdiAccountCircleOutline,
   mdiArrowLeft,
